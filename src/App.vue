df3114899f8a79dd15fe1bbc7711e814063d392b
WuCongSong <1491258957@qq.com>
2020-09-08 09:09:02 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view class="mianHome" />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="wap-home" :to="{name:'Home'}">首页</van-tabbar-item>
+      <van-tabbar-item icon="point-gift" :to="{name:'Order'}">热卖</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart" :to="{name:'Personal'}">购物车</van-tabbar-item>
+      <van-tabbar-item icon="friends" :to="{name:'Shopping'}">个人中心</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.mianHome {
+  flex: 1;
+  overflow: auto;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
 
-  const routes = [
+Vue.use(VueRouter);
+
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: () => import("../views/interface/Home.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/order",
+    name: "Order",
+    component: () => import("../views/interface/order.vue"),
+  },
+  {
+    path: "/personal",
+    name: "Personal",
+    component: () => import("../views/interface/personal.vue"),
+  },
+  {
+    path: "/shopping",
+    name: "Shopping",
+    component: () => import("../views/interface/shopping.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
